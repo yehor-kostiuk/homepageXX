2601f91f2428f49c28e101a62e3f58a94d366baf
Egor Kostiuk <kostukegor729@gmail.com>
2025-06-19 11:58:09 +0300

    feat: add Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import { ThemeToggle } from '@/components/ThemeToggle'
+
+export const HomePage = () => {
+	return (
+		<div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+			{/* Theme Toggle */}
+			<ThemeToggle />
+			{/* Background Effects */}
+
+			{/* Navbar */}
+
+			{/* Main Content */}
+
+			{/* Footer */}
+		</div>
+	)
+}
